cb65abd7e53400f042b20d230d0469f5f88cdf16
KatrinaB03-borrelll <897tddzz42@privaterelay.appleid.com>
2026-09-09 21:17:01 +0700

    Create Main.java
    Задан массив целых чисел: [1, 2, 3, 4, 5, 6, 7, 8, 9,10]  необходимо написать программу, которая выведет в консоль все чётные числа.

---
+++ task3/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 == 0) {
+        System.out.println("Четные числа " + arr[i]);
+      }
+    }
+    scanner.close();
+  }
+}
